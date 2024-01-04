f5614833837bc07208889ba516b3d9fdb935bc09
Giang Pham <ghpham25@colby.edu>
2024-01-04 17:49:32 +0000

    connected to mongodb

--- client/src/RecommendBookList.jsx
+++ client/src/RecommendBookList.jsx
@@ -1,7 +1,9 @@
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
+
 const extractBookRecommendations = (paragraph) => {
-  const pattern = /(\d+)\.\s+"([^"]+)"\s+by\s+([^:]+)[-:](.+?)(?=\d+\.|$)/gs;
+  const pattern = /(\d+)\.\s+"([^"]+)"\s+by\s+([^:]+)[\-:](.+?)(?=\d+\.|$)/gs;
   const matches = Array.from(paragraph.matchAll(pattern));
   console.log(matches);
 
@@ -23,15 +25,46 @@ export default function RecommendBookList() {
   const [recommendedBookList, setRecommendedBookList] = useState(
     extractBookRecommendations(recommendedBookListPara)
   );
+
+  const addToReadingList = async (title, author, description) => {
+    try {
+      console.log(`Adding book ${title} to reading list`);
+      const response = await axios.post("http://localhost:3000/api/add-to-reading-list", {
+        title: title,
+        author: author,
+        description: description,
+      });
+      console.log(response.data)
+    } catch (error) {
+      console.log("Error");
+    }
+  };
+
   return (
     <div>
       <h2>Book Recommendations</h2>
       <ul>
         {recommendedBookList.map((recommendation) => (
-          <li key={recommendation.order}>
-            <strong>{recommendation.title}</strong> by {recommendation.author}:{" "}
-            {recommendation.description}
-          </li>
+          <div className="book-rec">
+            <li key={recommendation.order}>
+              <strong>{recommendation.title}</strong> by {recommendation.author}
+              : {recommendation.description}
+              <div>
+                <button
+                  onClick={() =>
+                    addToReadingList(
+                      recommendation.title,
+                      recommendation.author,
+                      recommendation.description
+                    )
+                  }
+                >
+                  {" "}
+                  Add to Read list{" "}
+                </button>
+              </div>
+            </li>
+          </div>
         ))}
       </ul>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
+/* SET UP */
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const port = 3000;
+const { MongoClient } = require("mongodb");
 const { generateBooks } = require("./openai");
 require("dotenv").config();
+const port = 3000;
+const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* Connect to MongoDB */
+const mongodbPassword = process.env.MONGODB_PASSWORD;
+const uri = `mongodb+srv://giangpham25:${mongodbPassword}@cluster0.ow8rheo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(error) {
+    console.error("Error connecting to MongoDB:", error);
+}
+}
+run().catch(console.dir);
+
+/* APIs */
 app.post("/api/submit-book-preferences", async (req, res) => {
   try {
     const { bookMoods, similarBooks } = req.body;
@@ -16,6 +34,18 @@ app.post("/api/submit-book-preferences", async (req, res) => {
     res.json({ error: "Error submitting book preferences from openAI" });
   }
 });
+
+app.post("/api/add-to-reading-list", async(req, res) => {
+    try {
+        const {title, author, description} = req.body;
+        const readListDb = client.db("readList")
+        const readListCol = readListDb.collection("readListCol")
+        await readListCol.insertOne({title, author, description})
+        res.json({title, author, description})
+    } catch (error) {
+        res.json({ error: "Error adding book to read list" });
+    }
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
